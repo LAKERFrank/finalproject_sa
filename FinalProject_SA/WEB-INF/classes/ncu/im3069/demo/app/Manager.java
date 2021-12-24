fc9a6443c1ec6408a88d69c37da6bb12962f06c8
LAKERFrank <frank96270@gmail.com>
2021-12-24 15:28:59 +0800

    Manager push 1

---
+++ FinalProject_SA/WEB-INF/classes/ncu/im3069/demo/app/Manager.java
@@ -0,0 +1,23 @@
+package ncu.im3069.demo.app;
+
+import org.json.*;
+// TODO: Auto-generated Javadoc
+/**
+ * <p>
+ * The Class Manager
+ * Manager類別（class）具有管理員所需要之屬性與方法，並且儲存與管理員相關之商業判斷邏輯<br>
+ * </p>
+ * 
+ * @author IPLab
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+
+public class Manager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
